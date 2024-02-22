03f9ea2e36b1cd87ed72bdfd6df05055c0be9cb9
Raul Donde <glas109@gmail.com>
2024-02-22 00:31:18 -0600

    Terminar luego

--- MulticastServer.java
+++ MulticastServer.java
@@ -1,54 +1,135 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 
 public class MulticastServer {
     public static final int PORT = 6789;
+    private final MulticastSocket socket;
+    private final InetAddress group;
+    private final String name;
+    private final boolean isServer;
 
-    public MulticastServer(String name) {
-        System.out.println("Bienvenido " + name);
-        System.out.println("Escribe 'salir' para terminar la conversación");
+    private final HashMap<String, Integer> userPoints;
+    private final HashMap<String, Boolean> gotCorrectAnswer;
+    private JSONObject json;
 
+    public MulticastServer(String name, boolean isServer) throws Exception {
+        this.socket = new MulticastSocket(PORT);
+        this.group = InetAddress.getByName("224.0.0.1");
+        this.isServer = isServer;
+        this.userPoints = new HashMap<>();
+        this.gotCorrectAnswer = new HashMap<>();
+        String randomname = "Anónimo #" + (int) (Math.random() * 10000000);
+        this.name = name.isEmpty() || name.isBlank() ? randomname : name;
+        joinGroup();
+    }
+
+    private void joinGroup() throws Exception {
+        socket.joinGroup(group);
+        new Thread(this::receiveMessage).start();
+        announceJoinLeaveMessage(false);
+        if (isServer) {
+            new Thread(this::startGameService).start();
+        }
+    }
+
+    private void startGameService() {
         try {
-            MulticastSocket socket = new MulticastSocket(PORT);
-            InetAddress group = InetAddress.getByName("224.0.0.1");
-            socket.joinGroup(group);
-
-            new Thread(() -> recibirMensajes(socket)).start();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            boolean exit = false;
-            String message = "";
-            while (!exit) {
-                String input = br.readLine();
-                if (input.equals("exit")) {
-                    message = "El usuario: (" + name + ") ha salido de la conversación ";
-                    exit = true;
-                } else {
-                    message = name + ": " + input;
+            String contenido = new String(Files.readAllBytes(Paths.get("preguntas.json")));
+            json = new JSONObject(contenido);
+            JSONArray preguntas = json.getJSONArray("preguntas");
+            for (int i = 0; i < preguntas.length(); i++) {
+                JSONObject pregunta = preguntas.getJSONObject(i);
+                String enunciado = pregunta.getString("pregunta");
+                JSONArray opciones = pregunta.getJSONArray("opciones");
+                StringBuilder opcionesString = new StringBuilder();
+                for (int j = 0; j < opciones.length(); j++) {
+                    opcionesString.append("\n").append((j + 1)).append(". ").append(opciones.getString(j));
                 }
-                DatagramPacket data = new DatagramPacket(message.getBytes(), 0, message.length(), group, PORT);
-                socket.send(data);
+                String mensaje = enunciado + "...\n" + opcionesString;
+                sendMessage(mensaje);
+                Thread.sleep(10000);
+
             }
-            socket.leaveGroup(group);
-            socket.close();
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 
-    private void recibirMensajes(MulticastSocket socket) {
+    public void announceJoinLeaveMessage(boolean isExit) throws Exception {
+        String message = name + (isExit ? " ha salido del chat" : " ha entrado al chat");
+        DatagramPacket data = new DatagramPacket(message.getBytes(), 0, message.length(), group, PORT);
+        socket.send(data);
+    }
+
+    public void sendMessage(String message) throws Exception {
+        DatagramPacket data = new DatagramPacket(message.getBytes(), 0, message.length() + 1, group, PORT);
+        socket.send(data);
+    }
+
+    private void receiveMessage() {
         try {
             while (true) {
-                byte[] buffer = new byte[255];
+                byte[] buffer = new byte[256];
                 DatagramPacket data = new DatagramPacket(buffer, buffer.length);
                 socket.receive(data);
-                System.out.println(new String(data.getData(), 0, data.getLength()));
+                String receivedData = new String(data.getData(), data.getOffset(), data.getLength());
+                System.out.println(receivedData);
+                if (receivedData.equals("killserver") && isServer) {
+                    leaveGroup();
+                    return;
+                }
+                if (receivedData.contains("ha iniciado el juego")) {
+                    userPoints.clear();
+                    gotCorrectAnswer.clear();
+                    userPoints.put(name, 0);
+                    gotCorrectAnswer.put(name, false);
+                }
+                if (receivedData.contains("ha seleccionado la opción")) {
+                    String username = receivedData.split(",")[0];
+                    String option = receivedData.split(",")[1].split(":")[1].trim();
+                    if (getCurrentCorrectAnswer().equals(option)) {
+                        gotCorrectAnswer.put(username, true);
+                    } else {
+                        gotCorrectAnswer.put(username, false);
+                    }
+
+                    if (option.equals("4")) {
+                        addPointToUserIfItGotCorrectAnswer(username);
+                        printPoints();
+                    }
+
+                }
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    private String getCurrentCorrectAnswer() {
+        return "4";
+    }
+
+    private void addPointToUserIfItGotCorrectAnswer(String username) {
+        if (gotCorrectAnswer.get(username)) {
+            userPoints.put(username, userPoints.get(username) + 1);
+        }
+    }
+
+    private void printPoints() {
+        System.out.println("Puntajes:");
+        userPoints.forEach((k, v) -> System.out.println(k + ": " + v));
+    }
+
+    public void leaveGroup() throws Exception {
+        announceJoinLeaveMessage(true);
+        socket.leaveGroup(group);
+        socket.close();
     }
 }
--- Program.java
+++ Program.java
@@ -3,16 +3,67 @@ import java.io.InputStreamReader;
 
 class Program {
     public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("--server")) {
+            startServer();
+        } else {
+            startClient();
+        }
+    }
+
+    public static void startClient() {
         //Intenta unirse al grupo multicast, y si no, crea uno de fondo
+        MulticastServer server;
+        String nombre;
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Ingresa tu nombre de usuario...");
             System.out.print("Nombre: ");
-            String nombre = reader.readLine();
-            MulticastServer server = new MulticastServer(nombre);
+            nombre = reader.readLine();
+            server = new MulticastServer(nombre, false);
         } catch (Exception e) {
             System.out.println("No se pudo unir al grupo multicast");
+            return;
+        }
+        //Envía mensajes al grupo multicast
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String selectedNumber = reader.readLine();
+                switch (selectedNumber) {
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                        String message = nombre + ", ha seleccionado la opción:" + selectedNumber;
+                        server.sendMessage(message);
+                        break;
+                    case "empezar":
+                        server.sendMessage(nombre + " ha iniciado el juego");
+                        break;
+                    case "salir":
+                        server.leaveGroup();
+                        return;
+                    default:
+                        System.out.println("Opción no válida");
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
+    public static void startServer() {
+        MulticastServer server;
+        String nombre = "Croupier";
+        System.out.println("Server started");
+        try{
+            new MulticastServer(nombre, true);
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+
+    }
 }
